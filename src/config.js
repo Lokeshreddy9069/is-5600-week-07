3204281c7ca92bad48fa362b97139381dda9b24a
Lokeshreddy9069 <jangalokeshreddy@gmail.com>
2025-05-07 03:10:02 +0000

    chore: fix form

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // TODO: Replace this with your own Server URL for your codespace
 export const BASE_URL = "https://glowing-space-memory-q7p4jvr9g4p424rj7-3080.app.github.dev";
+export const BASE_URL = "https://sturdy-guacamole-v6q56pvq5w4qhp6wx-3080.app.github.dev";
